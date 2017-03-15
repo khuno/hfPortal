2e09512e17f6a399f8029c86e4a1757ef2f50aad
Sergii Khunovych <sergii@linux-ujcf.suse>
2017-03-15 23:02:44 +0100

    Add permission role for inserting to versions collection

--- imports/api/versions.js
+++ imports/api/versions.js
@@ -1,6 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
 import { check } from 'meteor/check';
+import { Roles } from 'meteor/alanning:roles';
 
 export const versions = new Mongo.Collection('versions');
 
@@ -12,7 +13,9 @@ Meteor.methods({
     if(! this.userId) {
       throw new Meteor.Error('not-authorized');
     }
-
+    if(! Roles.userIsInRole(this.userId, ['production'])) {
+      throw new Meteor.Error('not-permitted');
+    }
     versions.insert({
       value: obj.value,
       label: obj.label
